55b6fb8fe8b6b2294f6d88a986bbcc076e69d961
Francisco Penovi <fpenovi@users.noreply.github.com>
2020-05-06 20:11:13 -0300

    Added '/heavy' endpoint. (#4)
    Works with the same **t** parameter like /timeout.

---
+++ js/src/routes/heavy.js
@@ -0,0 +1,19 @@
+const { Router } = require('express');
+
+const SECOND = 1000;
+const DEFAULT_TIMEOUT = 5 * SECOND;
+
+const router = Router();
+router.get('/heavy', (req, res) => {
+    const time = (req.query.t && req.query.t * SECOND) || DEFAULT_TIMEOUT;
+    const start = new Date();
+
+    for (;;) {
+        let now = new Date();
+        if (now - start >= time) break;
+    }
+
+    res.send(`node - heavy: ${time}`);
+})
+
+module.exports = router;
