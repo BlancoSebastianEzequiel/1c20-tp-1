e48fe5cf1d72f51a4490aabc671fee42300e9122
Francisco Penovi <francisco.penovi@gmail.com>
2020-05-06 20:04:19 -0300

    Added '/heavy' endpoint.
    Works with the same **t** parameter like /timeout.

--- js/src/routes/index.js
+++ js/src/routes/index.js
@@ -1,5 +1,6 @@
 const PingRoute = require('./ping');
 const TimeoutRoute = require('./timeout');
+const HeavyRoute = require('./heavy');
 
 
-module.exports = [PingRoute, TimeoutRoute];
+module.exports = [PingRoute, TimeoutRoute, HeavyRoute];
