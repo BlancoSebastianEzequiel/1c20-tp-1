450872968553b978e6b33bf4ad84031b79367fdd
Francisco Penovi <francisco.penovi@gmail.com>
2020-05-07 15:26:53 -0300

    Removed unnecessary json conversion from ping endpoint

--- py/src/controllers/ping_controller.py
+++ py/src/controllers/ping_controller.py
@@ -1,10 +1,7 @@
-from flask import jsonify
 from flask_restful import Resource
 
 
 class PingController(Resource):
     @classmethod
     def get(cls):
-        res = jsonify("ping")
-        res.status_code = 200
-        return res
+        return 'ping'
